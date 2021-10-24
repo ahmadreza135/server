0a6493885c074cc747f7f72451d814ba2deb1757
ahmad <ahmadrezaqasemi1384@gmail.com>
2021-10-24 23:32:21 +0330

    hey this is three commit

--- serapp/admin.py
+++ serapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from serapp.models import emailv
+from serapp.models import emailv,room
 # Register your models here.
 
 admin.site.register(emailv)
+admin.site.register(room)
--- serapp/models.py
+++ serapp/models.py
@@ -7,4 +7,6 @@ class emailv(models.Model):
     email = models.CharField(max_length=200)
 
 class room(models.Model):
-    pass
+    name = models.CharField(max_length=32)
+    number_of_memebers = models.IntegerField()
+    
