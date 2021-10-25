07fc65d7d6648face972a5ba1343032cffe21e08
ahmad <ahmadrezaqasemi1384@gmail.com>
2021-10-25 21:05:47 +0330

    ahmdf

--- serapp/process/getrooboors.py
+++ serapp/process/getrooboors.py
@@ -3,9 +3,9 @@ from serapp.models import arz_roo_boors
 from django.views.decorators.csrf import csrf_protect ,csrf_exempt
 
 @csrf_exempt
-def get(request):
+def get_last(request):
     if request.method=='GET':
         g = arz_roo_boors.objects.all()
-        f = g.name
-        dat = {"name":"f"}
+        g = g[len(g)-1]
+        dat = {"name":g.name,"marketcap":g.market_cap,"price":g.price,"time":g.timeclosed}
         return JsonResponse(dat)
