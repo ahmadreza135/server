70f2d2beac30ee019d15d563d2e115e307ad32fe
ahmad <ahmadrezaqasemi1384@gmail.com>
2021-11-02 22:12:26 +0330

    sdlfj

--- serapp/process/login.py
+++ serapp/process/login.py
@@ -2,10 +2,12 @@ from django.views.decorators.csrf import csrf_protect ,csrf_exempt
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from json.encoder import JSONEncoder
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 import django
 
 @csrf_exempt
+# @csrf_protect
 def login_user(request):
     Password = request.POST.get('password')
     Username = request.POST.get("username")
@@ -29,11 +31,18 @@ def login_user(request):
             user.last_name = "true"
             user.save()
             data = {"login_user":"true"}
+            r = render(request,"../templates/login.html",context=data)
+            return r
         else:
             # data = {"isnone":"yes"}  
             data = {"login_user":"false"}     
     except django.contrib.auth.models.User.DoesNotExist: 
         data = {"user_exists": "false"}
     print (data,request.POST)
-    return JsonResponse(data,encoder=JSONEncoder)
+    # return JsonResponse(data,encoder=JSONEncoder)
+    r = render(request,"../templates/login.html",context=data)
+    for i in range(len(data)):
+        r.set_cookie(list(data.keys())[i],data[list(data.keys())[i]])
+    return r
+
     # return data["loggedin"]
